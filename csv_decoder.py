f980b12d26c6bc472212d2e9c27ba7308a4e4cb2
Kenneth Park <97331214+kamgaa@users.noreply.github.com>
2025-08-24 14:35:20 +0900

    08.19 update

--- csv_decoder.py
+++ csv_decoder.py
@@ -19,7 +19,7 @@ class CSVDecoderNode(Node):
         self.timer = self.create_timer(0.1, self.timer_callback)  # 10Hz
 
 
-        self.timer = self.create_timer(0.05, self.timer_callback)  # 20Hz
+        #self.timer = self.create_timer(0.05, self.timer_callback)  # 20Hz
 
         # 좌표 기준점 및 스케일링
         self.cf01_index = -1
@@ -40,46 +40,64 @@ class CSVDecoderNode(Node):
         # CSV 파일 로드
         package_share = get_package_share_directory('csv_decoder')
         #csv_path = os.path.join(package_share, 'config', 'SpaFrancorchamps.csv')
-        csv_path = os.path.join(package_share, 'config', 'figure8.csv')
+        #csv_path = os.path.join(package_share, 'config', 'figure8.csv')
         #csv_path = os.path.join(package_share, 'config', 'figure0.csv')
-        with open(csv_path, newline='', encoding='utf-8-sig') as csvfile:
-            reader = csv.reader(csvfile)
-            self.data = [(float(row[0]), float(row[1]),float(row[3])) for row in reader]
+        lead_path = os.path.join(package_share, 'config', 'figure8_lead.csv')
+        follower_path = os.path.join(package_share, 'config', 'figure8_foll.csv')
+        #with open(csv_path, newline='', encoding='utf-8-sig') as csvfile:
+        #    reader = csv.reader(csvfile)
+        #    self.data = [(float(row[0]), float(row[1]),float(row[3])) for row in reader]
+        with open(lead_path, newline='', encoding='utf-8-sig') as f1:
+            read_leader = csv.reader(f1)
+            self.data_lead = [(float(row[0]), float(row[1]), float(row[3])) for row in read_leader]
+        with open(follower_path, newline='', encoding='utf-8-sig') as f2:
+            read_follower = csv.reader(f2)
+            self.data_foll = [(float(row[0]), float(row[1]), float(row[3])) for row in read_follower]
 
     def trigger_callback(self, msg):
         if msg.data == 1111:
             self.get_logger().info('cf01 triggered')
             self.cf01_index = 0
+            self.cf01_arrived = False
         elif msg.data == 1100:
             self.get_logger().info('cf02 triggered')
-            self.cf02_index = 0    
+            self.cf02_index = 0
+            
+            self.cf02_arrived = False    
         
    
     def timer_callback(self):
         #elapsed = time.time() - self.start_time
         # cf01 도착 확인 및 index 초기화
-       
+        if self.cf01_arrived and not self.cf02_arrived:
+            self.get_logger().info('Leader arrived first, stopping follower')
+            self.cf02_arrived = True      
         # cf01 퍼블리시
-        if self.cf01_index >= 0 and self.cf01_index <= len(self.data):
-            x_raw, y_raw, yaw = self.data[self.cf01_index]
+        if not self.cf01_arrived and self.cf01_index >= 0 and self.cf01_index < len(self.data_lead):
+            x_raw, y_raw, yaw = self.data_lead[self.cf01_index]
             msg = Pose()
             msg.position.x = x_raw 
             msg.position.y = y_raw 
-            msg.position.z = 0.6
+            msg.position.z = 1.0
             msg.orientation.z = yaw
             self.cf01_pub.publish(msg)
             self.cf01_index += 1
+        elif not self.cf01_arrived and self.cf01_index >= 0 and self.cf01_index < len(self.data_lead):           # leader 경로 끝에 도달
+            self.cf01_arrived = True
 
         # cf02 퍼블리시
-        if self.cf02_index >= 0 and self.cf02_index <= len(self.data):
-            x_raw, y_raw, yaw = self.data[self.cf02_index]
+        if not self.cf02_arrived and self.cf02_index >= 0 and self.cf02_index < len(self.data_foll):
+            x_raw, y_raw, yaw = self.data_foll[self.cf02_index]
             msg = Pose()
             msg.position.x = x_raw 
             msg.position.y = y_raw
-            msg.position.z = 0.6
+            msg.position.z = 1.0
             msg.orientation.z = yaw
             self.cf02_pub.publish(msg)
             self.cf02_index += 1
+        elif not self.cf02_arrived and self.cf02_index >= 0 and self.cf02_index < len(self.data_foll): # follower 경로 끝에 도달
+            self.cf02_arrived = True
+
 
 
 def main(args=None):
